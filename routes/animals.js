58558bf8108126859dee52a537e5c6675b2e8c06
AdaN6 <a_disney_da@hotmail.com>
2022-11-20 21:34:42 +0100

    testing 1 - maping animal's name

--- routes/animals.js
+++ routes/animals.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const animals = require("../src/helper")
 
 
 router.get("/", (req, res) => {
@@ -8,11 +9,18 @@ router.get("/", (req, res) => {
 
 router
 .route("/:pet_type")
-.get((req,res) => {
-    const {pet_type} = req.params;
-res.render("pet_type", {
-  pet: `${pet_type.charAt(0).toUpperCase() + pet_type.slice(1)}`,
-});
+.get((req, res) => {
+  const { pet_type } = req.params;
+//   const html = `<ul>${animals[pet_type]
+//     .map((animal, index) => `<li>${animal.name}</li>`)
+//     .join("")}</ul>`;
+  
+  const html =  animals[pet_type].map((animal, index) => `<li>${animal.name}</li>`)
+
+  res.render("pet_type", {
+    pet: `${pet_type.charAt(0).toUpperCase() + pet_type.slice(1)}`,
+    name: `${html}`,
+  });
 })
 
 
