be4491862a6052752a93340e4f5ca72f200fdc62
AdaN6 <a_disney_da@hotmail.com>
2022-11-20 20:43:26 +0100

    created new animals route

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+//to read html file
+app.use(express.static("public"))
+
+//use view engine
+app.set("view engine", "ejs");
+
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("index");
 });
+
+//to have route in separate file
+const animalRouter = require('./routes/animals')
+
+app.use('/animals', animalRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
